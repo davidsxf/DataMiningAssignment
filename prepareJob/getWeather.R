b8b5277b75ee0384b7090d29f103aedb1d5af095
MengTianLv <MengTian_Lv@yeah.net>
2017-03-28 16:15:14 +0800

    根据城市的id获取天气 low 最低温度 high 最高温度 code 天气代码 humidity 湿度 visibility 能见度 pressure 气压 rising 气压变化

--- prepareJob/getWeather.R
+++ prepareJob/getWeather.R
@@ -9,24 +9,24 @@ getWeather <- function(woeid){
   #http://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid=2151330 and u='c'&format=xml
   #woeid <- 2151330
   #queryurl <- paste("http://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid=",woeid," and u='c'&format=xml",sep="")
+  #ans <- getNodeSet(doc,"//yweather:atmosphere")
   
   queryurl <- paste("http://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid=2151330"," and u='c'&format=xml",sep="")
   doc  <- xmlTreeParse(queryurl,useInternalNodes=TRUE)
   rootnode <- xmlRoot(doc) 
   
-  ans <- rootnode[[1]][[1]][[10]]  #yweather:atmosphere
-  #ans <- getNodeSet(doc,"//yweather:atmosphere")
-  humidity <- as.numeric(sapply(ans,xmlGetAttr,"humidity")) 
-  visibility <- as.numeric(sapply(ans,xmlGetAttr,"visibility")) 
-  pressure <- as.numeric(sapply(ans,xmlGetAttr,"pressure")) 
-  rising <- as.numeric(sapply(ans,xmlGetAttr,"rising")) 
+  ans <- rootnode[["results"]][["channel"]][["atmosphere"]]  #yweather:atmosphere
+  humidity <- xmlGetAttr(ans,"humidity") #温度
+  visibility <- xmlGetAttr(ans,"visibility") #能见度
+  pressure <- xmlGetAttr(ans,"pressure") #气压
+  rising <- xmlGetAttr(ans,"rising") #气压变化
   
-  ans <- getNodeSet(doc,"//item/yweather:condition")
-  code <- sapply(ans,xmlGetAttr,"code") 
+  ans <- rootnode[["results"]][["channel"]][["item"]][["condition"]]
+  code <- xmlGetAttr(ans,"code") #获取当前日期的天气编码
   
-  ans <- getNodeSet(doc,"//item/yweather:forecast[1]") 
-  low <- as.numeric(sapply(ans,xmlGetAttr,"low")) 
-  high <- as.numeric(sapply(ans,xmlGetAttr,"high")) 
+  ans <- rootnode[["results"]][["channel"]][["item"]][["forecast"]]
+  low <- xmlGetAttr(ans,"low")   #最低气温
+  high <- xmlGetAttr(ans,"high") #最高气温
   
   cbind(low,high,code,humidity,visibility,pressure,rising)
   
