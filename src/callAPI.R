5b6d17a75482157b812da2bf0acbea915b585bda
unknown <>
2017-04-06 19:01:10 +0800

    。。。。。。

--- src/callAPI.R
+++ src/callAPI.R
@@ -13,3 +13,4 @@ filepath = chartr("/","\\",getwd())
 wdata <- read.csv(file=paste(filepath,"\\weather\\","weather",getCurrentDate(),".csv",sep=""),header=TRUE,fileEncoding="utf-8",encoding="utf-8")#读取详细分类的天气
 recharts.emap <- createHTMLWithWeather(wdata = wdata,type="high",output=FALSE)
 recharts.emap
+
