fe218aeb7ca4df420fb43062a7b78dabb9a72fbd
MengTianLv <MengTian_Lv@yeah.net>
2017-04-13 11:10:11 +0800

    完善画图

--- achivement/src/achivement.R
+++ achivement/src/achivement.R
@@ -14,15 +14,16 @@ getDataSource <- function(){
   achivement <- read.transactions(getFilePath("achivement2"),format = "basket",sep=",")
   return (achivement)
 }
-#'根据支持度画图
+#'根据每个元素支持度画图
 paintItemFrequencyPlot <- function(support=0.3){
   png(filename=paste(getImgPath(),"频繁度.png"),width=1000,height=500)
   wdata <- getDataSource()
   itemFrequencyPlot(wdata,support=0.3)
   dev.off()
 }
-#'获取关联规则并写入文件
-getAndWriteRules <- function(support=0.2,confidence=0.2,minlen=2){
+
+#'获取关联规则
+getRules <- function(support=0.2,confidence=0.2,minlen=2){
   wdata <- getDataSource()
   achivementRules <- apriori(wdata,
                              parameter = list(
@@ -31,22 +32,61 @@ getAndWriteRules <- function(support=0.2,confidence=0.2,minlen=2){
                                minlen=minlen
                              ) )
   ordered_achivementrules <- sort(achivementRules, by="lift")
-  write(ordered_achivementrules, file=getFilePath("achivementRules"), sep=",", quote=TRUE, row.names=FALSE) 
+  return (ordered_achivementrules)
+}
+
+#
+dealWithRules <- function(support=0.2,confidence=0.2,minlen=2){
+  rules <- getRules(support,confidence,minlen)
+  writeData(rules,"achivement_rules")#
+  paintScatterPlot(rules,"Scatter_rules")
+  paintGroupedMatrix(rules,"Matrix_rules")
+  paintGraph(rules,"Graph_rules")
+}
+
+#'获取频繁项集
+getFreqItemSet <- function(support=0.2,minlen=2){
+  wdata <- getDataSource()
+  wdata.eclat <- eclat(wdata,parameter = list(
+    support=support,
+    minlen = minlen))
+  return (wdata.eclat)
+}
+#'
+dealWithFreqItemSet <- function(support=0.2,minlen=2){
+  freqItemSet <- getFreqItemSet(support,minlen)
+  writeData(freqItemSet,"achivement_freqItemSet")#
+  paintScatterPlot(freqItemSet,"Scatter_freqItemSet")
+  paintGraph(freqItemSet,"Graph_freqItemSet")
+}
+
+
+
+#'获取关联规则并写入文件
+writeData <- function(wdata,filename){
+  write(wdata, file=getFilePath(filename), sep=",", quote=TRUE, row.names=FALSE) 
 }
 #'绘制Scatter Plot
-paintScatterPlot <- function(){
-  png(filename=paste(getImgPath(),"Scatter.png"),width=1000,height=500)
-  wdata <- getDataSource()
-  achivementRules <- apriori(wdata,
-                             parameter = list(
-                               support=support,
-                               confidence=confidence,
-                               minlen=minlen
-                             ) )
-  ordered_achivementrules <- sort(achivementRules, by="lift")
-  plot(ordered_achivementrules)
+paintScatterPlot <- function(wdata,filename){
+  png(filename=paste(getImgPath(),filename,".png",sep=""),width=1000,height=500)
+  plot(wdata)
   dev.off()
 }
+
+#'绘制Grouped Matrix
+paintGroupedMatrix <- function(wdata,filename){
+  png(filename=paste(getImgPath(),filename,".png",sep=""),width=1000,height=500)
+  plot(wdata,method="grouped")
+  dev.off()
+}
+
+#'绘制graph
+paintGraph <- function(wdata,filename){
+  png(filename=paste(getImgPath(),filename,".png",sep=""),width=1000,height=500)
+  plot(wdata,method="graph",shading = "lift")
+  dev.off()
+}
+
 
 
 #'itemFreq <- itemFrequency(achivement) 每一个item出现的频率
